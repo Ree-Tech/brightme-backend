6d8627cec2e60158378c441655301677d3d34f9a
Bagas Raditya Nafi <bagasrnfull@gmail.com>
2023-10-05 20:30:19 +0700

    Feat : Add Model Migration Order

---
+++ app/Models/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Cart;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['updated_at'])->format('Y-m-d H:i:s');
+    }
+}
