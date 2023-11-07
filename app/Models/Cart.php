4abc90eb11e8b91f76f9e77f1d74369032cb4507
bagas <bagasrnfull@gmail.com>
2023-11-07 20:01:22 +0700

    Feat : Add Column Cast

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -14,6 +14,10 @@ class Cart extends Model
 
     protected $guarded = ['id'];
 
+    protected $casts = [
+        'is_check' => 'boolean',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
